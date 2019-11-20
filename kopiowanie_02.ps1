35992b4a73a4277f4bffe2833ccae743a40d8c1b
rafal-zloty <57992203+rafal-zloty@users.noreply.github.com>
2019-11-20 14:54:11 +0100

    Update kopiowanie_02.ps1
    Poprawki

--- kopiowanie_02.ps1
+++ kopiowanie_02.ps1
@@ -5,10 +5,12 @@ $przeliczeniaWorkbook = $przeliczeniaExcel.Workbooks.Open($baseDir + 'przeliczen
 $przeliczeniaWorksheet = $przeliczeniaWorkbook.WorkSheets.item('dane z pomiarow (V)')
 $przeliczeniaWorksheet.activate()
 
-#(Nazwa pliku z pomiarami, nazwa arkusza z pomiarami, zakres kolumn w arkuszu z pomiarami, komórke docelowa z pliku z przeliczeniami )
+#(Nazwa pliku z pomiarami, nazwa arkusza z pomiarami, zakres kolumn w arkuszu z pomiarami, komorka docelowa w pliku z przeliczeniami)
 $pomiaryList = @(@('pomiar1.xlsx','Arkusz1','A1:G1','A4'), @('pomiar2.xlsx','Arkusz1','A1:F1','A64'), @('pomiar2.xlsx','Arkusz1','G1','H4'), @('pomiar3.xlsx','Arkusz1','A1:F1','A124'), @('pomiar3.xlsx','Arkusz1','G1','I4'))
 
-ForEach ($pomiar in $pomiaryList){
+ForEach ($pomiar in $pomiaryList) {
+  "Kopiowanie danych z pliku {0}, arkusza {1} z kolumn {2}, do komorki {3}" -f $pomiar[0], $pomiar[1], $pomiar[2], $pomiar[3] | Out-Host
+
   $pomiarExcel = New-Object -ComObject Excel.Application
   $pomiarWorkbook = $pomiarExcel.Workbooks.Open($baseDir + $pomiar[0])
   $pomiarWorksheet = $pomiarWorkbook.WorkSheets.item($pomiar[1])
